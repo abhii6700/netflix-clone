40d9cc93c36ca1e731517cb453f44b2c158f1a73
abhii6700 <abhishekyadav6700.ay@gmail.com>
2021-05-28 20:54:04 +0530

    fixed API issue

--- src/app/pages/homepage/index.jsx
+++ src/app/pages/homepage/index.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
         <Banner />
         <Row
             title='NETFLIX ORIGINALS'
-            fetchURL={requests.fetchRomanceMovies}
+            fetchURL={requests.fetchNetflixOriginals}
             isLargeRow
         />
         <Row title='Trending Now' fetchURL={requests.fetchTrending} />
